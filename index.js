634f889474f5792d801d1709589626a5d1b57828
fov42550564 <42550564@qq.com>
2020-08-28 23:26:56 +0800

    初次提交

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const ReactNative = require('react-native');
+const {
+    NativeModules,
+} = ReactNative;
+
+const RCTImageEdit = NativeModules.RCTImageEdit;
+
+export async function edit(uri, actions = [], { format = 'jpeg', ...rest } = {}) {
+    return await RCTImageEdit.edit(uri, actions, { format, ...rest });
+}
+
+module.exports = {
+    edit,
+};
